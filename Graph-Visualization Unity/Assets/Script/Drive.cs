4d87b014081db18dd26ea2a1ba1c799c00e1f91f
kbamuza <bkulani@gmail.com>
2017-07-27 10:07:11 +0200

    Files for sync and keyboard control

---
+++ Graph-Visualization Unity/Assets/Script/Drive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Drive : MonoBehaviour {
+
+    // Use this for initialization
+    /*void Start () {
+        
+	}*/
+
+    float speed = 10.0f;
+    float rotationSpeed = 100.0f;
+
+    // Update is called once per frame
+    void Update () {
+        float translation = CrossPlatformInputManager.GetAxis("Vertical") * speed;
+        float rotation = CrossPlatformInputManager.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+	}
+}
---
+++ Graph-Visualization Unity/Assets/Script/SetupLocalPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+    [SyncVar] //tells server to sync(one way) variable out to all clients
+    public string Pname = "player";
+    public void OnGUI()
+    {
+        if (isLocalPlayer)
+            Pname = GUI.TextField(new Rect(25, Screen.height - 40, 100, 30), Pname);
+    }
+
+    // Use this for initialization
+    public void Start () {
+		if(isLocalPlayer)
+        {
+            GetComponent<Drive>().enabled = true;
+        }
+	}
+
+    public void Update()
+    {
+        if (isLocalPlayer)
+            this.GetComponentInChildren<TextMesh>().text = Pname;
+    }
+
+}
