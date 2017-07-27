91effe0ba64f36baff0d6e7fb3b6cbc464136383
Sigama_Takalani <sigamatakalani@gmail.com>
2017-07-27 20:22:41 +0200

    Rotate fixed

---
+++ Graph-Visualization-Unity/Assets/Script/Graph.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public class Graph : MonoBehaviour {
+
+    public void createGraph(List<GameObject> vertexList, List<EdgePairs> pairsList, GameObject nodePrefab)
+    {
+        string json = File.ReadAllText("Assets/Graphs/graph.json");
+
+        Edge[] tempEdgeList = JsonHelper.FromJson<Edge>(json);
+
+        GameObject parent;
+        GameObject child;
+
+
+        foreach (Edge edge in tempEdgeList)
+        {
+            if (!vertexList.Any(x => x.name == edge.parent))
+            {
+                parent = Instantiate(nodePrefab) as GameObject;
+                parent.name = edge.parent;
+                parent.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f));
+                vertexList.Add(parent);
+            }
+            else
+            {
+                parent = vertexList.Where(x => x.name == edge.parent).FirstOrDefault();
+            }
+
+            if (!vertexList.Any(x => x.name == edge.child))
+            {
+                child = Instantiate(nodePrefab) as GameObject;
+                child.name = edge.child;
+                child.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f));
+                vertexList.Add(child);
+            }
+            else
+            {
+                child = vertexList.Where(x => x.name == edge.child).FirstOrDefault();
+            }
+
+            //Add new edge to graph
+            EdgePairs tempEdge = new EdgePairs(parent, child, edge.value);
+            pairsList.Add(tempEdge);
+            //connectTwoNodesDraw(parent, child);
+        }
+    }
+
+    public void createGraph2(List<GameObject> vertexList, List<EdgePairs> pairsList, GameObject nodePrefab)
+    {
+        string json = File.ReadAllText("Assets/Graphs/graph.json");
+
+        Edge[] tempEdgeList = JsonHelper.FromJson<Edge>(json);
+
+        //GameObject parent;
+        //GameObject child;
+
+
+        foreach (Edge edge in tempEdgeList)
+        {
+            if (!vertexList.Any(x => x.name == edge.parent))
+            {
+                GameObject parent = Instantiate(nodePrefab) as GameObject;
+                parent.name = edge.parent;
+                parent.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f));
+                vertexList.Add(parent);
+              //  parent.
+            }
+
+            if (!vertexList.Any(x => x.name == edge.child))
+            {
+                GameObject child = Instantiate(nodePrefab) as GameObject;
+                child.name = edge.child;
+                child.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f));
+                vertexList.Add(child);
+                Destroy(child);
+            }
+
+            //Add new edge to graph
+            
+        }
+
+        foreach (Edge edge in tempEdgeList)
+        {
+
+            EdgePairs tempEdge = new EdgePairs(vertexList.Where(U => U.name == edge.parent).FirstOrDefault(), vertexList.Where(U => U.name == edge.child).FirstOrDefault(), edge.value);
+            pairsList.Add(tempEdge);
+            //connectTwoNodesDraw(parent, child);
+        }
+    }
+}
+
+
+//helper classes
+[System.Serializable]
+public class Edge
+{
+    public string parent;
+    public int value;
+    public string child;
+
+    public Edge()
+    {
+        parent = "";
+        value = 0;
+        child = "";
+    }
+}
+
+public class EdgePairs
+{
+    public GameObject parent;
+    public GameObject child;
+    public int value;
+
+    public EdgePairs()
+    {
+        parent = new GameObject();
+        child = new GameObject();
+    }
+
+    public EdgePairs(GameObject parent1, GameObject child1, int value1)
+    {
+
+        parent = parent1;
+        child = child1;
+        value = value1;
+    }
+}
+
+//json helper class to create list of edges
+public static class JsonHelper
+{
+    public static T[] FromJson<T>(string json)
+    {
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+        return wrapper.items;
+    }
+
+    public static string ToJson<T>(T[] array)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.items = array;
+        return JsonUtility.ToJson(wrapper);
+    }
+
+    public static string ToJson<T>(T[] array, bool prettyPrint)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.items = array;
+        return JsonUtility.ToJson(wrapper, prettyPrint);
+    }
+
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] items;
+    }
+}
--- Graph-Visualization-Unity/Assets/graphStart.cs
+++ Graph-Visualization-Unity/Assets/graphStart.cs
@@ -10,11 +10,14 @@ public class graphStart : MonoBehaviour {
 
     //default cylinder mesh
     public Mesh cylinderMesh;
+    public GameObject graphObject;
 
     public GameObject[] points;
 
     Transform nodeTransform;
-    
+
+    float speedMod = 2.0f;
+
     public GameObject nodePrefab;
     public GameObject edgePrefab;
     private List<Vector3> usedPositions = new List<Vector3>();
@@ -31,103 +34,22 @@ public class graphStart : MonoBehaviour {
         pairsList = new List<EdgePairs>();
         vertexList = new List<GameObject>();
 
-        string json = File.ReadAllText("Assets/Graphs/graph.json");
+        Graph graph = new Graph();
+        graph.createGraph(vertexList, pairsList, nodePrefab);
 
-        Edge[] tempEdgeList = JsonHelper.FromJson<Edge>(json);
-
-        foreach (Edge edge in tempEdgeList)
+        //connect vertecies
+        foreach (EdgePairs edge in pairsList)
         {
-            
-            GameObject parent = Instantiate(nodePrefab) as GameObject;
-            parent.name = edge.parent;
-       //     parent.transform.position = Vector3();
-
-            GameObject child = Instantiate(nodePrefab) as GameObject;
-            child.name = edge.child;
-
-            //Add new edge to graph
-            EdgePairs tempEdge = new EdgePairs(parent, child, edge.value);
-            pairsList.Add(tempEdge);
-
-            //add edge connections;
-            connectTwoNodesDraw(parent, child);
-
-            bool insertParent = true, insertChild = true;
-
-            //add to vertex list if not exists
-            foreach (GameObject vertex in vertexList)
-            {
-                if(vertex.name == edge.parent)
-                {
-                    insertParent = false;
-                }
-
-                if(vertex.name == edge.child)
-                {
-                    insertChild = false;
-                }
-            }
-
-            if(insertParent)
-            {
-                vertexList.Add(parent);
-            }
-
-            if(insertChild)
-            {
-                vertexList.Add(child);
-            }
+            Debug.Log(edge.parent.name + " and " + edge.child.name);
+           connectTwoNodesDraw(edge.parent, edge.child);
         }
 
-        //GameObject node1 = Instantiate(nodePrefab) as GameObject;
-        //node1.transform.position = new Vector3(1, 1, 1);
-        //node1.name = "node1";
-        ////node1.tag = "1";
-
-        //GameObject node2 = Instantiate(nodePrefab) as GameObject;
-        //node2.transform.position = new Vector3(4, 1, 1);
-        //node2.name = "node2";
-        //// node2.tag = "2";
-
-        //GameObject node3 = Instantiate(nodePrefab) as GameObject;
-        //node3.transform.position = new Vector3(2, 4, 3);
-        //node3.name = "node3";
 
 
-        //GameObject node4 = Instantiate(nodePrefab) as GameObject;
-        //node4.transform.position = new Vector3(7, 6, 3);
-        //node4.name = "node4";
 
-        //GameObject node5 = Instantiate(nodePrefab) as GameObject;
-        //node5.transform.position = new Vector3(-7, 1, 2);
-        //node5.name = "node5";
-
-        //connectTwoNodesDraw(node1,node2);
-        //EdgePairs tmp = new  EdgePairs(node1, node2, 10);
-        //pairsList.Add(tmp);
-        
-        //connectTwoNodesDraw(node2, node3);
-        //EdgePairs tmp2 = new EdgePairs(node2, node3, 20);
-        //pairsList.Add(tmp2);
-
-        //connectTwoNodesDraw(node3, node1);
-        //EdgePairs tmp3 = new EdgePairs(node3, node1, 30);
-        //pairsList.Add(tmp3);
+        transform.LookAt(new Vector3(0, 0, 0));
 
 
-        //connectTwoNodesDraw(node3, node4);
-        //EdgePairs tmp4 = new EdgePairs(node3, node4, 40);
-        //pairsList.Add(tmp4);
-
-        //connectTwoNodesDraw(node3, node5);
-        //EdgePairs tmp5 = new EdgePairs(node3, node5, 50);
-        //pairsList.Add(tmp5);
-        
-        ////hello
-
-        //connectTwoNodesDraw(node5, node2);
-        //EdgePairs tmp6 = new EdgePairs(node5, node2, 60);
-        //pairsList.Add(tmp6);
     }
 
     /// <summary>
@@ -150,6 +72,7 @@ public class graphStart : MonoBehaviour {
         float cylinderDistance = 0.5f * Vector3.Distance(node1.transform.position, node2.transform.position);
         Debug.Log(" here " + cylinderDistance);
          node2.transform.parent = this.gameObject.transform;
+        node1.transform.parent = this.gameObject.transform;
 
         // We make a offset gameobject to counteract the default cylindermesh pivot/origin being in the middle
         GameObject ringOffsetCylinderMeshObject = new GameObject();
@@ -195,87 +118,57 @@ public class graphStart : MonoBehaviour {
             count++;
         }
 
+        graphObject.transform.RotateAround(new Vector3(0, 0, 0), new Vector3(0.0f, 1.0f, 0.0f), 20 * Time.deltaTime * speedMod);
+
     }
 
-    public int nodeCounter = 0;
-    public Vector3 PositionAlgorithm()
-    {
-        Vector3 nodePosi = new Vector3 (0, 0, 0);
-        nodeCounter++;
-        int x = 0;
-        int y = 0;
-        int z = 0;
-        if(nodeCounter < 10)
-        {
+    //GameObject node1 = Instantiate(nodePrefab) as GameObject;
+    //node1.transform.position = new Vector3(1, 1, 1);
+    //node1.name = "node1";
+    ////node1.tag = "1";
 
-        }
-        return nodePosi;
-    }
-}
+    //GameObject node2 = Instantiate(nodePrefab) as GameObject;
+    //node2.transform.position = new Vector3(4, 1, 1);
+    //node2.name = "node2";
+    //// node2.tag = "2";
 
-[System.Serializable]
-public class Edge
-{
-    public string parent;
-    public int value;
-    public string child;
+    //GameObject node3 = Instantiate(nodePrefab) as GameObject;
+    //node3.transform.position = new Vector3(2, 4, 3);
+    //node3.name = "node3";
 
-    public Edge() {
-        parent = "";
-        value = 0;
-        child = "";
-    }
-}
 
-public class EdgePairs
-{
-    public GameObject parent;
-    public GameObject child;
-    public int value;
+    //GameObject node4 = Instantiate(nodePrefab) as GameObject;
+    //node4.transform.position = new Vector3(7, 6, 3);
+    //node4.name = "node4";
 
-    public EdgePairs()
-    {
-        parent = new GameObject();
-        child = new GameObject();
-    }
+    //GameObject node5 = Instantiate(nodePrefab) as GameObject;
+    //node5.transform.position = new Vector3(-7, 1, 2);
+    //node5.name = "node5";
 
-    public EdgePairs(GameObject parent1, GameObject child1, int value1)
-    {
+    //connectTwoNodesDraw(node1,node2);
+    //EdgePairs tmp = new  EdgePairs(node1, node2, 10);
+    //pairsList.Add(tmp);
 
-        parent = parent1;
-        child = child1;
-        value = value1;
-    }
-}
+    //connectTwoNodesDraw(node2, node3);
+    //EdgePairs tmp2 = new EdgePairs(node2, node3, 20);
+    //pairsList.Add(tmp2);
 
-//json helper class to create list of edges
-public static class JsonHelper
-{
-    public static T[] FromJson<T>(string json)
-    {
-        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
-        return wrapper.items;
-    }
+    //connectTwoNodesDraw(node3, node1);
+    //EdgePairs tmp3 = new EdgePairs(node3, node1, 30);
+    //pairsList.Add(tmp3);
 
-    public static string ToJson<T>(T[] array)
-    {
-        Wrapper<T> wrapper = new Wrapper<T>();
-        wrapper.items = array;
-        return JsonUtility.ToJson(wrapper);
-    }
 
-    public static string ToJson<T>(T[] array, bool prettyPrint)
-    {
-        Wrapper<T> wrapper = new Wrapper<T>();
-        wrapper.items = array;
-        return JsonUtility.ToJson(wrapper, prettyPrint);
-    }
+    //connectTwoNodesDraw(node3, node4);
+    //EdgePairs tmp4 = new EdgePairs(node3, node4, 40);
+    //pairsList.Add(tmp4);
 
-    [System.Serializable]
-    private class Wrapper<T>
-    {
-        public T[] items;
-    }
+    //connectTwoNodesDraw(node3, node5);
+    //EdgePairs tmp5 = new EdgePairs(node3, node5, 50);
+    //pairsList.Add(tmp5);
+
+    //connectTwoNodesDraw(node5, node2);
+    //EdgePairs tmp6 = new EdgePairs(node5, node2, 60);
+    //pairsList.Add(tmp6);
 }
 
 
