58efbd69abcb5fe1ef9c5d02caf2ce486d61b5a8
Takalani_Sigama <sigamatakalani@gmail.com>
2017-08-10 17:34:03 +0200

    Testing Framework update

---
+++ Testing/UnitTestAppsynth1/UnitTestAppsynth1/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestAppsynth1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestClass]
+        public class RomanNumeralConverterUpperAndLowerBoundsUnitTests
+        {
+
+            [TestMethod]
+            [ExpectedException(typeof(IndexOutOfRangeException))]
+            public void Start_TestMethod()
+            {
+                var converter = new RomanNumeralConverter();
+                converter.ConvertRomanNumeral(3001);
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(IndexOutOfRangeException))]
+            public void connectTwoNodesDraw_TestMethod()
+            {
+                var converter = new RomanNumeralConverter();
+                converter.ConvertRomanNumeral(3001);
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(IndexOutOfRangeException))]
+            public void Update_TestMethod()
+            {
+                var converter = new RomanNumeralConverter();
+                converter.ConvertRomanNumeral(3001);
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(IndexOutOfRangeException))]
+            public void createGraph()
+            {
+                var converter = new RomanNumeralConverter();
+                converter.ConvertRomanNumeral(3001);
+            }
+        }
+    }
+}
