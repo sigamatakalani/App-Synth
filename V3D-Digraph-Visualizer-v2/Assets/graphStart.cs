fd7dbb0d701f1b95dfd47d539bbf4ddfa2ba2536
Munyaradzi Mpofu <munyabenyera@gmail.com>
2017-08-25 17:34:50 +0200

    Json graph changes

--- V3D-Digraph-Visualizer-v2/Assets/graphStart.cs
+++ V3D-Digraph-Visualizer-v2/Assets/graphStart.cs
@@ -89,159 +89,162 @@ public class graphStart : MonoBehaviour {
 
         /*************************************************************************Begin Test Code*******************************************************/
 
-        GameObject node1 = Instantiate(nodePrefab) as GameObject;
-        node1.name = "node1";
-        node1.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
-        node1.AddComponent<LineRenderer>();
-        node1.AddComponent<Dragable>();
-        node1.GetComponent<Rigidbody>().useGravity = false;
-        node1.GetComponent<Rigidbody>().drag = 3;
-        node1.transform.parent = transform;
-        vertexList.Add(node1);
+        //GameObject node1 = Instantiate(nodePrefab) as GameObject;
+        //node1.name = "node1";
+        //node1.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
+        //node1.AddComponent<LineRenderer>();
+        //node1.AddComponent<Dragable>();
+        //node1.GetComponent<Rigidbody>().useGravity = false;
+        //node1.GetComponent<Rigidbody>().drag = 3;
+        //node1.transform.parent = transform;
+        //vertexList.Add(node1);
 
-        GameObject node2 = Instantiate(nodePrefab) as GameObject;
-        node2.name = "node2";
-        node2.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
-        node2.AddComponent<LineRenderer>();
-        node2.AddComponent<Dragable>();
-        node2.GetComponent<Rigidbody>().useGravity = false;
-        node2.GetComponent<Rigidbody>().drag = 3;
-        node2.transform.parent = transform;
-        vertexList.Add(node2);
+        //GameObject node2 = Instantiate(nodePrefab) as GameObject;
+        //node2.name = "node2";
+        //node2.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
+        //node2.AddComponent<LineRenderer>();
+        //node2.AddComponent<Dragable>();
+        //node2.GetComponent<Rigidbody>().useGravity = false;
+        //node2.GetComponent<Rigidbody>().drag = 3;
+        //node2.transform.parent = transform;
+        //vertexList.Add(node2);
 
-        GameObject node3 = Instantiate(nodePrefab) as GameObject;
-        node3.name = "node3";
-        node3.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
-        node3.AddComponent<LineRenderer>();
-        node3.AddComponent<Dragable>();
-        node3.GetComponent<Rigidbody>().useGravity = false;
-        node3.GetComponent<Rigidbody>().drag = 3;
-        node3.transform.parent = transform;
-        vertexList.Add(node3);
+        //GameObject node3 = Instantiate(nodePrefab) as GameObject;
+        //node3.name = "node3";
+        //node3.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
+        //node3.AddComponent<LineRenderer>();
+        //node3.AddComponent<Dragable>();
+        //node3.GetComponent<Rigidbody>().useGravity = false;
+        //node3.GetComponent<Rigidbody>().drag = 3;
+        //node3.transform.parent = transform;
+        //vertexList.Add(node3);
 
-        GameObject node4 = Instantiate(nodePrefab) as GameObject;
-        node4.name = "node4";
-        node4.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
-        node4.AddComponent<LineRenderer>();
-        node4.AddComponent<Dragable>();
-        node4.GetComponent<Rigidbody>().useGravity = false;
-        node4.GetComponent<Rigidbody>().drag = 3;
-        node4.transform.parent = transform;
-        vertexList.Add(node4);
+        //GameObject node4 = Instantiate(nodePrefab) as GameObject;
+        //node4.name = "node4";
+        //node4.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
+        //node4.AddComponent<LineRenderer>();
+        //node4.AddComponent<Dragable>();
+        //node4.GetComponent<Rigidbody>().useGravity = false;
+        //node4.GetComponent<Rigidbody>().drag = 3;
+        //node4.transform.parent = transform;
+        //vertexList.Add(node4);
 
-        GameObject node5 = Instantiate(nodePrefab) as GameObject;
-        node5.name = "node5";
-        node5.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
-        node5.AddComponent<LineRenderer>();
-        node5.AddComponent<Dragable>();
-        node5.GetComponent<Rigidbody>().useGravity = false;
-        node5.GetComponent<Rigidbody>().drag = 3;
-        node5.transform.parent = transform;
-        vertexList.Add(node5);
+        //GameObject node5 = Instantiate(nodePrefab) as GameObject;
+        //node5.name = "node5";
+        //node5.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
+        //node5.AddComponent<LineRenderer>();
+        //node5.AddComponent<Dragable>();
+        //node5.GetComponent<Rigidbody>().useGravity = false;
+        //node5.GetComponent<Rigidbody>().drag = 3;
+        //node5.transform.parent = transform;
+        //vertexList.Add(node5);
 
-        GameObject node6 = Instantiate(nodePrefab) as GameObject;
-        node6.name = "node6";
-        node6.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
-        node6.AddComponent<LineRenderer>();
-        node6.AddComponent<Dragable>();
-        node6.GetComponent<Rigidbody>().useGravity = false;
-        node6.GetComponent<Rigidbody>().drag = 3;
-        node6.transform.parent = transform;
-        vertexList.Add(node6);
+        //GameObject node6 = Instantiate(nodePrefab) as GameObject;
+        //node6.name = "node6";
+        //node6.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
+        //node6.AddComponent<LineRenderer>();
+        //node6.AddComponent<Dragable>();
+        //node6.GetComponent<Rigidbody>().useGravity = false;
+        //node6.GetComponent<Rigidbody>().drag = 3;
+        //node6.transform.parent = transform;
+        //vertexList.Add(node6);
 
-        GameObject node7 = Instantiate(nodePrefab) as GameObject;
-        node7.name = "node7";
-        node7.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
-        node7.AddComponent<LineRenderer>();
-        node7.AddComponent<Dragable>();
-        node7.GetComponent<Rigidbody>().useGravity = false;
-        node7.GetComponent<Rigidbody>().drag = 3;
-        node7.transform.parent = transform;
-        vertexList.Add(node7);
+        //GameObject node7 = Instantiate(nodePrefab) as GameObject;
+        //node7.name = "node7";
+        //node7.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
+        //node7.AddComponent<LineRenderer>();
+        //node7.AddComponent<Dragable>();
+        //node7.GetComponent<Rigidbody>().useGravity = false;
+        //node7.GetComponent<Rigidbody>().drag = 3;
+        //node7.transform.parent = transform;
+        //vertexList.Add(node7);
 
-        pairsList.Add(new EdgePairs(node1, node2, 22));
-        pairsList.Add(new EdgePairs(node2, node3, 22));
-        pairsList.Add(new EdgePairs(node2, node4, 22));
-        pairsList.Add(new EdgePairs(node2, node5, 22));
-        pairsList.Add(new EdgePairs(node3, node5, 22));
-        pairsList.Add(new EdgePairs(node4, node5, 22));
-        pairsList.Add(new EdgePairs(node5, node6, 22));
-        pairsList.Add(new EdgePairs(node7, node4, 22));
+        //pairsList.Add(new EdgePairs(node1, node2, 22));
+        //pairsList.Add(new EdgePairs(node2, node3, 22));
+        //pairsList.Add(new EdgePairs(node2, node4, 22));
+        //pairsList.Add(new EdgePairs(node2, node5, 22));
+        //pairsList.Add(new EdgePairs(node3, node5, 22));
+        //pairsList.Add(new EdgePairs(node4, node5, 22));
+        //pairsList.Add(new EdgePairs(node5, node6, 22));
+        //pairsList.Add(new EdgePairs(node7, node4, 22));
 
-        //SpringJoint joint = node1.AddComponent<SpringJoint>();
-        //joint.minDistance = Vector3.Distance(node1.transform.position, node2.transform.position);
-        //joint.maxDistance = Vector3.Distance(node1.transform.position, node2.transform.position);
-        //node1.GetComponent<SpringJoint>().connectedBody = node2.GetComponent<Rigidbody>();
+        ////SpringJoint joint = node1.AddComponent<SpringJoint>();
+        ////joint.minDistance = Vector3.Distance(node1.transform.position, node2.transform.position);
+        ////joint.maxDistance = Vector3.Distance(node1.transform.position, node2.transform.position);
+        ////node1.GetComponent<SpringJoint>().connectedBody = node2.GetComponent<Rigidbody>();
 
-        foreach (EdgePairs edge in pairsList)
-        {
-            line = edge.parent.GetComponent<LineRenderer>();
-            line.startWidth = 0.05f;
-            line.endWidth = 0.05f;
-            line.positionCount = 2;
-            line.GetComponent<Renderer>().enabled = true;
-            line.SetPosition(0, edge.parent.transform.position);
-            line.SetPosition(1, edge.child.transform.position);
-            line.material = new Material(Shader.Find("Sprites/Default"));
-            line.startColor = Color.white;
-            line.endColor = Color.white;
-        }
+        //foreach (EdgePairs edge in pairsList)
+        //{
+        //    line = edge.parent.GetComponent<LineRenderer>();
+        //    line.startWidth = 0.05f;
+        //    line.endWidth = 0.05f;
+        //    line.positionCount = 2;
+        //    line.GetComponent<Renderer>().enabled = true;
+        //    line.SetPosition(0, edge.parent.transform.position);
+        //    line.SetPosition(1, edge.child.transform.position);
+        //    line.material = new Material(Shader.Find("Sprites/Default"));
+        //    line.startColor = Color.white;
+        //    line.endColor = Color.white;
+        //}
 
         /*************************************************************************End Test Code*******************************************************/
 
         //string json = File.ReadAllText("./Assets/Graphs/graph.json");
 
-        //Edge[] tempEdgeList = JsonHelper.FromJson<Edge>(json);
+        string json = GameObject.Find("InformationObject").GetComponent<InformationScript>().jsonToSend;
+        Debug.Log(json);
 
-        //foreach (Edge edge in tempEdgeList)
-        //{
-        //    if (!vertexList.Any(x => x.name == edge.parent))
-        //    {
-        //        GameObject parent = Instantiate(nodePrefab) as GameObject;
-        //        parent.name = edge.parent;
-        //        parent.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
-        //        parent.AddComponent<LineRenderer>();
-        //        parent.AddComponent<Dragable>();
-        //        parent.GetComponent<Rigidbody>().useGravity = false;
-        //        parent.GetComponent<Rigidbody>().drag = 3;
-        //        parent.transform.parent = transform;
-        //        vertexList.Add(parent);
-        //    }
+        Edge[] tempEdgeList = JsonHelper.FromJson<Edge>(json);
 
-        //    if (!vertexList.Any(x => x.name == edge.child))
-        //    {
-        //        GameObject child = Instantiate(nodePrefab) as GameObject;
-        //        child.name = edge.child;
-        //        child.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
-        //        child.AddComponent<LineRenderer>();
-        //        child.AddComponent<Dragable>();
-        //        child.GetComponent<Rigidbody>().useGravity = false;
-        //        child.GetComponent<Rigidbody>().drag = 3;
-        //        child.transform.parent = transform;
-        //        vertexList.Add(child);
-        //    }
-        //}
+        foreach (Edge edge in tempEdgeList)
+        {
+            if (!vertexList.Any(x => x.name == edge.parent))
+            {
+                GameObject parent = Instantiate(nodePrefab) as GameObject;
+                parent.name = edge.parent;
+                parent.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
+                parent.AddComponent<LineRenderer>();
+                parent.AddComponent<Dragable>();
+                parent.GetComponent<Rigidbody>().useGravity = false;
+                parent.GetComponent<Rigidbody>().drag = 3;
+                parent.transform.parent = transform;
+                vertexList.Add(parent);
+            }
 
-        //foreach (Edge edge in tempEdgeList)
-        //{
-        //    EdgePairs tempEdge = new EdgePairs(vertexList.Where(U => U.name == edge.parent).FirstOrDefault(), vertexList.Where(U => U.name == edge.child).FirstOrDefault(), edge.value);
-        //    //tempEdge.parent.AddComponent<Rigidbody>();
-        //    //tempEdge.child.AddComponent<Rigidbody>();
-        //    //SpringJoint joint = tempEdge.parent.AddComponent<SpringJoint>();
-        //    //joint.connectedBody = tempEdge.child.GetComponent<Rigidbody>();
+            if (!vertexList.Any(x => x.name == edge.child))
+            {
+                GameObject child = Instantiate(nodePrefab) as GameObject;
+                child.name = edge.child;
+                child.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
+                child.AddComponent<LineRenderer>();
+                child.AddComponent<Dragable>();
+                child.GetComponent<Rigidbody>().useGravity = false;
+                child.GetComponent<Rigidbody>().drag = 3;
+                child.transform.parent = transform;
+                vertexList.Add(child);
+            }
+        }
 
-        //    LineRenderer line = tempEdge.parent.GetComponent<LineRenderer>();
-        //    line.startWidth = 0.05f;
-        //    line.endWidth = 0.05f;
-        //    line.positionCount = 2;
-        //    line.GetComponent<Renderer>().enabled = true;
-        //    line.SetPosition(0, tempEdge.parent.transform.position);
-        //    line.SetPosition(1, tempEdge.child.transform.position);
-        //    line.material = new Material(Shader.Find("Particles/Additive"));
-        //    line.startColor = Color.white;
-        //    line.endColor = Color.white;
-        //    pairsList.Add(tempEdge);
-        //}
+        foreach (Edge edge in tempEdgeList)
+        {
+            EdgePairs tempEdge = new EdgePairs(vertexList.Where(U => U.name == edge.parent).FirstOrDefault(), vertexList.Where(U => U.name == edge.child).FirstOrDefault(), edge.value);
+            //tempEdge.parent.AddComponent<Rigidbody>();
+            //tempEdge.child.AddComponent<Rigidbody>();
+            //SpringJoint joint = tempEdge.parent.AddComponent<SpringJoint>();
+            //joint.connectedBody = tempEdge.child.GetComponent<Rigidbody>();
+
+            LineRenderer line = tempEdge.parent.GetComponent<LineRenderer>();
+            line.startWidth = 0.05f;
+            line.endWidth = 0.05f;
+            line.positionCount = 2;
+            line.GetComponent<Renderer>().enabled = true;
+            line.SetPosition(0, tempEdge.parent.transform.position);
+            line.SetPosition(1, tempEdge.child.transform.position);
+            line.material = new Material(Shader.Find("Particles/Additive"));
+            line.startColor = Color.white;
+            line.endColor = Color.white;
+            pairsList.Add(tempEdge);
+        }
 
         Debug.Log("Field of view: " + Camera.main.fieldOfView);
 
