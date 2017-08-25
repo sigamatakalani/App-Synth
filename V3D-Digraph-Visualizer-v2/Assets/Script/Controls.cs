7e91e95eda2d87b3b877f482f47ef6c7b572eb7e
Munyaradzi Mpofu <munyabenyera@gmail.com>
2017-08-25 11:43:21 +0200

    Some ui work
    I added some ui controls to the graph scene. Need to attach them to the relevant functionality

---
+++ V3D-Digraph-Visualizer-v2/Assets/Script/Controls.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+        graphStart graph = GetComponent<graphStart>();
+        GameObject.FindWithTag("RotateLeft");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
